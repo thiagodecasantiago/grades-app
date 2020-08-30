8d4c3d85baaddbdb891dd65768e4f9a4ac177f85
Thiago Santiago <thyoga@gmail.com>
2020-08-30 00:35:24 -0300

    Change back-end connection URL

--- src/http-common.js
+++ src/http-common.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 //Define a URL base da origem para consumo do servico
 export default axios.create({
-  baseURL: 'http://localhost:8081/',
+  baseURL: process.env.REACT_APP_API_URL,
   headers: {
     'Content-type': 'application/json',
   },
